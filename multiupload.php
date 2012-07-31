df088ae951ad8631fa683ba832b4380668a57d8e
Bade Iriabho <ebade@yahoo.com>
2012-07-31 17:18:07 -0500

    modified API calls to behave appropriately

--- multiupload.php
+++ multiupload.php
@@ -71,18 +71,35 @@
 		if($key != '') {
 			//Check for filename and generate the filekey
 			include_once('core.header.php');
-			if(isset($_REQUEST['item_id'])) {
+			if(isset($_REQUEST['collection_id'])) {
+				$result = array();
 				$s3ql=compact('user_id','db');
 				$s3ql['select']='*';
-				$s3ql['from']='statements';
-				$s3ql['where']['item_id']=$_REQUEST['item_id'];
+				$s3ql['from']='items';
+				$s3ql['where']['collection_id']=$_REQUEST['collection_id'];
 				$s3ql['format']=$_REQUEST['format'];
+				$res = S3QLaction($s3ql);
 				
-				$done = S3QLaction($s3ql);
+				$count = count($res);
+				if($count > 0) {
+					for($i = 0; $i < $count; $i++) {
+						$s3ql=compact('user_id','db');
+						$s3ql['select']='*';
+						$s3ql['from']='statements';
+						$s3ql['where']['item_id']=$res[$i]['item_id'];
+						$s3ql['format']=$_REQUEST['format'];
+						$res2 = S3QLaction($s3ql);
+						
+						$result = array_merge($result,$res2);
+					}
+				}
+				//add apikey entry
+				array_walk($result,create_function('&$item','$item["apikey"] = '.$key.';'));
+
 				if(strtolower($format) == 'json') {
-					echo json_remove_callback(formatReturn(0,compact('done'),$format,''));
+					echo json_remove_callback(formatReturn(0,compact('result'),$format,''));
 				} else {
-					echo formatReturn(0,compact('done'),$format,'');
+					echo formatReturn(0,compact('result'),$format,'');
 				}
 			}
 		} else {
@@ -147,31 +164,48 @@
 										                         
 						if ($moved) {
 							//Was a rule_id and item_id provided? If yes, insert it using s3ql, if not, provide the filekey
-							if($_REQUEST['item_id'] && $_REQUEST['rule_id']){
+							//if($_REQUEST['item_id'] && $_REQUEST['rule_id']){
+							if($_REQUEST['collection_id'] && $_REQUEST['rule_id']){
+								$itemid = 0;
+								//create new item for file
 								$s3ql=compact('user_id','db');
-								$s3ql['insert']='file';
-								$s3ql['where']['item_id']=$_REQUEST['item_id'];
-								$s3ql['where']['rule_id']=$_REQUEST['rule_id'];
-								$s3ql['where']['filekey']=$filekey;
-				
-								$s3ql['format']=$_REQUEST['format'];
-				
-								$done = S3QLaction($s3ql);
-								$msg .=$done;
-								if(strtolower($format) == 'json') {
-									// file_size file_name download_url statement_id
-									preg_match('/(?<=\"file_id\"\:)\d+/',$msg,$statementid);
-									$statementid = $statementid[0];
-									$tmpMsg = ',"file_name":"'.$filename.'","file_size":"'.$filesize.'","statement_id":"'.$statementid.'","download_url":"'.S3DB_URI_BASE.'/download.php?key='.$key.'&statement_id='.$statementid.'"';
-									$tmpPos = strpos($msg, ',');
-									if($tmpPos !== false) {
-										if($tmpPos > 0) {
-											$msg = substr($msg, 0, $tmpPos).$tmpMsg.substr($msg, $tmpPos);
+								$s3ql['insert']='item';
+								$s3ql['where']['collection_id']=$_REQUEST['collection_id'];
+								$s3ql['where']['notes']=$filename;
+								$s3ql['format']='json';
+								$res = S3QLaction($s3ql);
+								
+								//extract item ID
+								preg_match( '/(?:\"item_id\"|\'item_id\') *:* *(?:\"|\')* *([a-zA-Z0-9]+) *(?:\"|\')*/i', $res, $matches);
+								if(is_array($matches) && count($matches) > 1) {
+									$itemid = trim($matches[1]);
+									
+									$s3ql=compact('user_id','db');
+									$s3ql['insert']='file';
+									$s3ql['where']['item_id']=$itemid;
+									$s3ql['where']['rule_id']=$_REQUEST['rule_id'];
+									$s3ql['where']['filekey']=$filekey;
+									$s3ql['format']=$_REQUEST['format'];
+									$done = S3QLaction($s3ql);
+
+									$msg .=$done;
+									if(strtolower($format) == 'json') {
+										// file_size file_name download_url statement_id
+										preg_match('/(?<=\"file_id\"\:)\d+/',$msg,$statementid);
+										$statementid = $statementid[0];
+										$tmpMsg = ',"file_name":"'.$filename.'","file_size":"'.$filesize.'","statement_id":"'.$statementid.'","apikey":"'.$key.'","download_url":"'.S3DB_URI_BASE.'/download.php?key='.$key.'&statement_id='.$statementid.'"';
+										$tmpPos = strpos($msg, ',');
+										if($tmpPos !== false) {
+											if($tmpPos > 0) {
+												$msg = substr($msg, 0, $tmpPos).$tmpMsg.substr($msg, $tmpPos);
+											}
 										}
 									}
+								} else {
+									$msg .= formatReturn('3',"File did not save properly, please try again.",$format,'');
 								}
 							} else {
-								$msg .= formatReturn('0',"This filekey is to be used instead of key for file transfer, it will expire in 24h. Break the file in base64 encoded fragments, replacing the character '+' with it's URL equivalent '%2b'",$format,array('filekey'=>$filekey));
+								$msg .= formatReturn('3',"You did not provide a Collection and Rule ID",$format,'');
 							}
 						} else {
 							$msg .= formatReturn('2',"Failed to import file ".$filename,$format,'');
@@ -184,23 +218,11 @@
 					}
 				}
 			}
-			//if there is no data besides the key, ask for a filename and filesize
-	
-			//if($filename == '') $filename = $xml -> filename;
-			//$filesize = $_REQUEST['filesize'];
-			//if($filesize == '') $filesize = $xml -> filesize;
-					
-			//Copy file from Php tmp directory
 		} else {		//if key is empty, check for filekey
 			$filekey = $xml->filekey;
 			if ($filekey == '') $filekey = $_REQUEST['filekey'];
 			if ($filekey!='') {
 				include_once('core.filekeyheader.php');
-	
-				//add a form to the page such that it accepts both POST and GET
-				//echo '<form name="file" method="POST">';
-				//echo '<input type="hidden" name="query"> <!-- this form is for programming environments that support sending POST -->';
-				//echo '</form>';
 	
 				//echo receiveFileFragments(compact('filekey', 'db'));
 				if(strtolower($format) == 'json') {
@@ -246,16 +268,44 @@
 			if(isset($_REQUEST['statement_id'])) {
 				preg_match('/\d+/', $_REQUEST['statement_id'], $statementid);
 				$statementid = $statementid[0];
-				$s3ql=compact('user_id','db');
-				$s3ql['delete']='statement';
-				$s3ql['where']['statement_id']=$_REQUEST['statement_id'];
-				$s3ql['format']=$_REQUEST['format'];
-		
-				$done = S3QLaction($s3ql);
-				if(strtolower($format) == 'json') {
-					echo json_remove_callback(formatReturn(0,compact('done'),$format,''));
+				
+				//check if the statement ID is the only one that belongs to its item
+				$s3ql = compact('user_id','db');
+				$s3ql['select']='*';
+				$s3ql['from']='statements';
+				$s3ql['where']['statement_id']=$statementid;
+				$res = S3QLaction($s3ql);
+				
+				if(isset($res[0]['item_id']) && intval($res[0]['item_id']) > 0) {
+					$itemid = $res[0]['item_id'];
+					$s3ql = compact('user_id','db');
+					$s3ql['select']='*';
+					$s3ql['from']='statements';
+					$s3ql['where']['item_id']=$itemid;
+					$res = S3QLaction($s3ql);
+					
+					//delete the statement
+					$s3ql=compact('user_id','db');
+					$s3ql['delete']='statement';
+					$s3ql['where']['statement_id']=$statementid;
+					$s3ql['format']=$_REQUEST['format'];
+					$done = S3QLaction($s3ql);
+					
+					//determine whether to delete item
+					if(count($res) == 1) {
+						$s3ql=compact('user_id','db');
+						$s3ql['delete']='item';
+						$s3ql['where']['item_id']=$itemid;
+						$done2 = S3QLaction($s3ql);
+					}
+					
+					if(strtolower($format) == 'json') {
+						echo json_remove_callback(formatReturn(0,compact('done'),$format,''));
+					} else {
+						echo formatReturn(0,compact('done'),$format,'');
+					}
 				} else {
-					echo formatReturn(0,compact('done'),$format,'');
+					echo formatReturn('3',"Could not find the item the statement belongs to.",$format,''); exit;
 				}
 			}
 		} else {
@@ -265,7 +315,6 @@
 				echo formatReturn('3',"Key is missing.",$format,''); exit;
 			}
 		}
-		
 	}
 	
 	function json_remove_callback($str) {
